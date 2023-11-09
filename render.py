ca2e6752d6835536ba698ba2e448ea8b7af7b53c
Zan Gojcic <zgojcic@nvidia.com>
2023-11-09 22:49:28 +0100

    initial render script - just nerf dataset

--- render.py
+++ render.py
@@ -1,46 +1,93 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import argparse
+import torch
+import torchvision
+import os
+import numpy as np
 
-from threedgrut.render import Renderer
+from tqdm import tqdm
+from model import MixtureOfGaussians
+from datasets.utils import move_to_gpu
+
+from datasets.colmap_dataset import ColmapDataset
+from datasets.nerf_dataset import NeRFDataset
+from datasets.ngp_dataset import NGPDataset
+from datasets.ncore_dataset import NCoreDataset
+
+from torchmetrics import PeakSignalNoiseRatio
 
 if __name__ == "__main__":
     # Set up command line argument parser
     parser = argparse.ArgumentParser()
     parser.add_argument("--checkpoint", required=True, type=str, help="path to the pretrained checkpoint")
-    parser.add_argument(
-        "--path", type=str, default="", help="Path to the training data, if not provided taken from ckpt"
-    )
     parser.add_argument("--out-dir", required=True, type=str, help="Output path")
-    parser.add_argument(
-        "--save-gt", action="store_false", help="If set, the GT images will not be saved [True by default]"
-    )
-    parser.add_argument(
-        "--compute-extra-metrics",
-        action="store_false",
-        help="If set, extra image metrics will not be computed [True by default]",
-    )
+    parser.add_argument("--save-gt", action="store_false", help="If set, the GT images will not be saved [True by default]")
     args = parser.parse_args()
 
-    renderer = Renderer.from_checkpoint(
-        checkpoint_path=args.checkpoint,
-        path=args.path,
-        out_dir=args.out_dir,
-        save_gt=args.save_gt,
-        computes_extra_metrics=args.compute_extra_metrics,
-    )
 
-    renderer.render_all()
+    checkpoint = torch.load(args.checkpoint)
+    conf = checkpoint["config"]
+    global_step = checkpoint['global_step']
+
+    val_collate_fn = None
+    # Create the dataset
+    if conf.dataset.type == 'nerf':
+        dataset = NeRFDataset(
+            conf.path, 
+            split='test', 
+            sample_full_image=conf.dataset.train.sample_full_image, 
+            batch_size=conf.dataset.train.batch_size,
+            return_alphas=True
+        )
+    else:
+        raise ValueError(f'Unsupported dataset type: {conf.dataset.type}. Choose between: ["colmap", "nerf", "ngp", "ncore"]. ')
+    
+    dataloader = torch.utils.data.DataLoader(dataset, num_workers=1, batch_size=1, shuffle=False, collate_fn=val_collate_fn)
+
+    # Initialize the model and the optix context
+    model = MixtureOfGaussians(conf)
+    model.set_optix_context()
+
+    # Initialize the parameters from checkpoint
+    model.init_from_checkpoint(checkpoint)
+    model.build_bvh()
+
+    # Criterions that we log during training
+    criterions = {"psnr":  PeakSignalNoiseRatio(data_range=1).to("cuda")}
+
+    output_path_renders = os.path.join(args.out_dir, f"ours_{int(global_step)}", "renders")
+    os.makedirs(output_path_renders, exist_ok=True)
+
+    if args.save_gt:
+        output_path_gt = os.path.join(args.out_dir, f"ours_{int(global_step)}", "gt")
+        os.makedirs(output_path_gt, exist_ok=True)
+    
+    psnr = []
+    iteration = 0
+    with tqdm(dataloader) as pbar:
+        for batch in pbar:
+            with torch.no_grad():
+                gpu_batch = move_to_gpu(batch)
+                rays_ori, rays_dir, rgb_gt = gpu_batch["rays_ori"], gpu_batch["rays_dir"], gpu_batch["rgb_gt"]
+                # Compute the outputs of a single batch
+                outputs = model(rays_ori, rays_dir)
+                # Compute the loss
+                psnr.append(criterions["psnr"](outputs['pred_rgb'], rgb_gt).item())
+                pbar.set_postfix({'iteration': iteration, 'psnr': psnr[-1]})
+                
+                # Store the images
+                rgb_pred = outputs['pred_rgb']
+                if "alphas" in gpu_batch:
+                    rgb_pred = torch.cat([rgb_pred, outputs['pred_opacity']], dim=-1)
+
+                torchvision.utils.save_image(rgb_pred.squeeze(0).permute(2,0,1), os.path.join(output_path_renders, '{0:05d}'.format(iteration) + ".png"))
+                if args.save_gt:
+                    if "alphas" in gpu_batch: 
+                        rgb_gt = torch.cat([rgb_gt, gpu_batch["alpha"]], dim=-1)
+
+                    gt_data = torch.cat([rgb_gt, gpu_batch["alpha"]], dim=-1)
+                    torchvision.utils.save_image(rgb_gt.squeeze(0).permute(2,0,1), os.path.join(output_path_gt, '{0:05d}'.format(iteration) + ".png"))
+
+
+                iteration += 1
+
+        print(f"PSNR : {np.mean(psnr)}, std: f{np.std(psnr)}")
